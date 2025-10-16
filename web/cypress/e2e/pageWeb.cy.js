230a799faeed2975fab05dfe0dfe797be0047193
Ericton Brito <erictonbrito@outlook.com>
2025-10-16 15:16:35 -0400

    melhorando o codigo pageWeb e removendo alguns codigos comentados

--- web/cypress/e2e/pageWeb.cy.js
+++ web/cypress/e2e/pageWeb.cy.js
@@ -5,42 +5,37 @@ const home = homePage;
 
 describe('Realizar busca de um livro específico e adicioná-lo ao carrinho', () => {
   
-  it('Acessar página inicial da Amazon', () => {
-    home.goTo();            // Acessar página inicial da Amazon
+  beforeEach(() => {
+    home.goTo(); // Acessar página inicial da Amazon
   });
 
-  it('Pesquisar pelo livro “AI Engineering: Building Applications with Foundation Models', ()=> {
-    home.goTo();            // Acessar página inicial da Amazon
+  it('Pesquisar pelo livro “AI Engineering: Building Applications with Foundation Models', () => {
     home.bookSearch();      // Pesquisa pelo titulo
   });
 
   it('Selecionar o livro correto', () => {
-    home.goTo();            // Acessar página inicial da Amazon
     home.bookSearch();      // Pesquisa pelo titulo
     home.selectBook();      // Lista de resultados
   });
 
-  it('Validar detalhes do livro', ()=> {
-    home.goTo();            // Acessar página inicial da Amazon
+  it('Validar detalhes do livro', () => {
     home.bookSearch();      // Pesquisa pelo titulo
     home.selectBook();      // Lista de resultados
     home.bookDetails();     // Validar detalhes do livro
   });
-  it('Adicionar o livro ao carrinho', ()=> {
-    home.goTo();            // Acessar página inicial da Amazon
+
+  it('Adicionar o livro ao carrinho', () => {
     home.bookSearch();      // Pesquisa pelo titulo
     home.selectBook();      // Lista de resultados
     home.bookDetails();     // Validar detalhes do livro
     home.addBookToCart();   // Adicionar o livro ao carrinho
   });
 
-  it('Fluxo completo da busca do livro especifico', ()=> {
-    home.goTo();                  // Acessar página inicial da Amazon
+  it('Fluxo completo da busca do livro especifico', () => {
     home.bookSearch();            // Pesquisa pelo titulo
     home.selectBook();            // Lista de resultados
     home.bookDetails();           // Validar detalhes do livro
     home.addBookToCart();         // Adicionar o livro ao carrinho
     home.confirmationMessage();   // Validar mensagem de confirmação
   });
-  
 });
